47403863776c63e895717093fc6b29f2de3df54f
Seongbin Kim <seongbin9786@gmail.com>
2018-04-25 23:25:12 +0900

    거품 정렬 구현 (이전 것은 구현 안된 버전임)

--- 2_정렬_알고리즘_구현/3_거품_정렬/bubble_sort.js
+++ 2_정렬_알고리즘_구현/3_거품_정렬/bubble_sort.js
@@ -14,22 +14,28 @@ try {
 */
 function bubble_sort(list) {
   info("거품 정렬로 " + list + " 배열을 정렬합니다.");
+  info("앞에 있는 원소가 뒤의 원소보다 클 경우 교환합니다.");
 
   if (list.length === 0) throw new RangeError("list 배열의 크기가 0입니다.");
   if (!(list instanceof Array) || list === undefined || list === null)
     throw new TypeError("list 매개변수는 반드시 배열이어야 합니다.");
 
   let temp;
-  let minIdx;
+  // 요소 갯수만큼 순회해야한다.
   for (let i = 0; i < list.length; i++) {
-    minIdx = i;
-    for (let j = i + 1; j < list.length; j++) {
-      if (list[j] < list[minIdx]) minIdx = j;
+    // j는 항상 1번부터 시작.
+    // 스왑되었든 안되었든, 다음 j번째에서 비교
+    // 이렇게 1회 순회하면 된 것임.
+    for (let j = 1; j < list.length - i; j++) {
+      // j-1번째보다 작으면 스왑.
+      if (list[j - 1] > list[j]) {
+        draw_list(list, j, j - 1);
+        temp = list[j];
+        list[j] = list[j - 1];
+        list[j - 1] = temp;
+      }
     }
-    draw_list(list, i, minIdx);
-    temp = list[i];
-    list[i] = list[minIdx];
-    list[minIdx] = temp;
   }
+
   success("거품 정렬에 성공했습니다: " + list);
 }
